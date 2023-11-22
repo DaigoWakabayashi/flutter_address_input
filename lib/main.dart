7ccee2e81c4331d4dc9169b265b68a4febb47462
DaigoWakabayashi <mamushi.journey@gmail.com>
2023-11-22 23:34:18 +0300

    feat: 基盤となるページ作成

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,50 @@ import 'package:flutter_address_input/firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MainApp());
+  runApp(const App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return const MaterialApp(home: ListPage());
+  }
+}
+
+class ListPage extends StatelessWidget {
+  const ListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text('$runtimeType'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (_) => const AddPage(),
+            fullscreenDialog: true,
+          ),
         ),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class AddPage extends StatelessWidget {
+  const AddPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text('$runtimeType'),
       ),
     );
   }
