ca19a283b948a8abba03c07c8d5572340aa01a60
DaigoWakabayashi <mamushi.journey@gmail.com>
2023-11-23 00:56:42 +0300

    feat: 一覧表示

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_address_input/firebase_options.dart';
 import 'package:flutter_address_input/pages/list_page.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const App());
+  runApp(const ProviderScope(child: App()));
 }
 
 class App extends StatelessWidget {
--- lib/pages/list_page.dart
+++ lib/pages/list_page.dart
@@ -1,13 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_address_input/models/address.dart';
 import 'package:flutter_address_input/pages/add_page.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class ListPage extends StatelessWidget {
+final _addressesStreamProvider = StreamProvider<List<Address>>((ref) {
+  return FirebaseFirestore.instance.collection('addresses').snapshots().map(
+        (snapshot) =>
+            snapshot.docs.map((doc) => Address.fromJson(doc.data())).toList(),
+      );
+});
+
+class ListPage extends ConsumerWidget {
   const ListPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final stream = ref.watch(_addressesStreamProvider);
     return Scaffold(
-      body: Center(child: Text('$runtimeType')),
+      body: stream.when(
+        data: (addresses) {
+          return ListView.builder(
+            itemCount: addresses.length,
+            itemBuilder: (_, index) {
+              final address = addresses[index];
+              return ListTile(
+                title: Text(address.address1),
+                subtitle: Text(address.address2 + address.address3),
+              );
+            },
+          );
+        },
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (error, stackTrace) => Center(child: Text(error.toString())),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.of(context).push(
           MaterialPageRoute(
