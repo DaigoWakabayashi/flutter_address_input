07da8e20ff457438c5c3f4ccce774ed3366ca420
DaigoWakabayashi <mamushi.journey@gmail.com>
2023-11-24 19:31:24 +0300

    feat: 郵便番号の有効な形式は数字7桁のみに絞る

--- lib/pages/add_page.dart
+++ lib/pages/add_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gap/gap.dart';
 
@@ -75,8 +76,13 @@ class AddPage extends HookWidget {
             TextFormField(
               controller: zipcodeController,
               focusNode: zipcodeFocusNode,
-              decoration: const InputDecoration(labelText: '郵便番号'),
+              decoration: const InputDecoration(labelText: '郵便番号(7桁)'),
               onEditingComplete: () => prefcodeFocusNode.requestFocus(),
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                LengthLimitingTextInputFormatter(7),
+                FilteringTextInputFormatter.digitsOnly,
+              ],
             ),
             const Gap(8),
             TextFormField(
